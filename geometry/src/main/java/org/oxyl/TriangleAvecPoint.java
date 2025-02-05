4072fd8dceacaa10d3212da359f05127f4b71b2e
DorianLn <dorian.lonegro@epfedu.fr>
2025-02-05 09:34:21 +0100

    test

--- geometry/src/main/java/org/oxyl/TriangleAvecPoint.java
+++ geometry/src/main/java/org/oxyl/TriangleAvecPoint.java
@@ -4,6 +4,7 @@ public class TriangleAvecPoint {
     private Point point1;
     private Point point2;
     private Point point3;
+    private Point point4;
 
     public TriangleAvecPoint(Point p1, Point p2, Point p3) {
         this.point1 = new Point(p1);
