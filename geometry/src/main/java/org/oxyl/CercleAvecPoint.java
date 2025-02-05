557553fa6629e96e1b1b9e8782159a9eae99cdf1
DorianLn <dorian.lonegro@epfedu.fr>
2025-02-05 10:47:08 +0100

    ajout de la méthode CercleAvecPoint

--- geometry/src/main/java/org/oxyl/CercleAvecPoint.java
+++ geometry/src/main/java/org/oxyl/CercleAvecPoint.java
@@ -3,10 +3,18 @@ package org.oxyl;
 public class CercleAvecPoint {
     private Point point1;
     private Point point2;
+    private double rayon;
 
-    public Cercle(){
+    public CercleAvecPoint(Point point1, Point point2, double rayon) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.rayon = rayon;
+    }
+
+    public CercleAvecPoint(){
         point1 = new Point(0,0);
         point2 = new Point(0,0);
+        rayon = 1.0;
     }
 
 
